703c2e0c2dab9a864f1d942374b9a6118a18347e
Alex Vye <alexvye@gmail.com>
2012-07-18 12:32:23 -0300

    added toolbar to sellviewcontroller

--- XCraftShow2/SellViewController_ipod.m
+++ XCraftShow2/SellViewController_ipod.m
@@ -391,14 +391,14 @@ ProductSelectorTableViewController* prodView;
     [audioPlayer play];                                 
 }
 
--(IBAction) selectProduct: (UIButton*) aButton {
+-(IBAction) selectProduct:(id)sender {
     // 
     // Pass the selected object to the new view controller.
     //
     [self.navigationController pushViewController:prodView animated:YES];
 }
 
--(IBAction) editProduct: (UIButton*) aButton {
+-(IBAction) editProduct:(id)sender {
 	//
 	// push the view controller
 	//
@@ -411,6 +411,62 @@ ProductSelectorTableViewController* prodView;
     // Pass the selected object to the new view controller.
     //
     [self.navigationController pushViewController:addView animated:YES];
+}
+
+-(IBAction)openMail:(id)sender {
+    if ([MFMailComposeViewController canSendMail])
+    {
+        MFMailComposeViewController *mailer = [[MFMailComposeViewController alloc] init];
+        
+        mailer.mailComposeDelegate = self;
+        
+        [mailer setSubject:@"A Message from MobileTuts+"];
+        
+        NSArray *toRecipients = [NSArray arrayWithObjects:@"fisrtMail@example.com", @"secondMail@example.com", nil];
+        [mailer setToRecipients:toRecipients];
+        
+        UIImage *myImage = [UIImage imageNamed:@"mobiletuts-logo.png"];
+        NSData *imageData = UIImagePNGRepresentation(myImage);
+        [mailer addAttachmentData:imageData mimeType:@"image/png" fileName:@"mobiletutsImage"]; 
+        
+        NSString *emailBody = @"Have you seen the MobileTuts+ web site?";
+        [mailer setMessageBody:emailBody isHTML:NO];
+        
+        [self presentModalViewController:mailer animated:YES];  
+        
+    } else {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Failure"
+                                                        message:@"Your device doesn't support the composer sheet"
+                                                       delegate:nil
+                                              cancelButtonTitle:@"OK"
+                                              otherButtonTitles:nil];
+        [alert show];
+    }
+}
+
+- (void)mailComposeController:(MFMailComposeViewController*)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError*)error 
+{   
+    switch (result)
+    {
+        case MFMailComposeResultCancelled:
+            NSLog(@"Mail cancelled: you cancelled the operation and no email message was queued.");
+            break;
+        case MFMailComposeResultSaved:
+            NSLog(@"Mail saved: you saved the email message in the drafts folder.");
+            break;
+        case MFMailComposeResultSent:
+            NSLog(@"Mail send: the email message is queued in the outbox. It is ready to send.");
+            break;
+        case MFMailComposeResultFailed:
+            NSLog(@"Mail failed: the email message was not saved or queued, possibly due to an error.");
+            break;
+        default:
+            NSLog(@"Mail not sent.");
+            break;
+    }
+    
+    // Remove the mail view
+    [self dismissModalViewControllerAnimated:YES];
 }
 
 @end
