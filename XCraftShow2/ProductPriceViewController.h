75ace96a8ec41690d886299eaeee4aa8d4c8a8e7
Peter Chase <cchasep@hotmail.com>
2012-07-18 20:25:03 -0300

    Added new view controller.

---
+++ XCraftShow2/ProductPriceViewController.h
@@ -0,0 +1,22 @@
+//
+//  ProductPriceViewControllerViewController.h
+//  XCraftShow2
+//
+//  Created by Peter Chase on 12-07-17.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#define kDollars    0
+#define KCents      1
+
+@interface ProductPriceViewController : UIViewController<UIPickerViewDataSource, UIPickerViewDelegate>
+
+@property(nonatomic, strong) IBOutlet UILabel* priceLabel;
+@property(nonatomic, strong) UILabel* prevPriceLabel;
+
+- (IBAction)cancel:(id)sender;
+- (IBAction)save:(id)sender;
+
+@end
---
+++ XCraftShow2/ProductPriceViewController.m
@@ -0,0 +1,79 @@
+//
+//  ProductPriceViewController.m
+//  XCraftShow2
+//
+//  Created by Peter Chase on 12-07-17.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ProductPriceViewController.h"
+
+@interface ProductPriceViewController ()
+
+@end
+
+@implementation ProductPriceViewController
+@synthesize priceLabel = _priceLabel;
+@synthesize prevPriceLabel = _prevPriceLabel;
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+    if (self.prevPriceLabel != nil) {
+        self.priceLabel.text = self.prevPriceLabel.text;
+    }
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+- (IBAction)cancel:(id)sender {
+    [self dismissModalViewControllerAnimated:YES];
+}
+
+- (IBAction)save:(id)sender {
+    self.prevPriceLabel.text = self.priceLabel.text;
+    [self dismissModalViewControllerAnimated:YES];
+}
+
+#pragma mark - UIPickerViewDataSource methods
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
+    return 2;
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
+    if (component == kDollars) {
+        return 1000;
+    }
+    
+    return 100;
+}
+
+#pragma mark - UIPickerViewDelegate methods
+- (NSString*)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component {
+    if (component == kDollars) {
+        return [NSString stringWithFormat:@"%d", row];
+    }
+
+    return [NSString stringWithFormat:@"%02d", row];
+}
+
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component {
+    NSInteger dollarValue = [pickerView selectedRowInComponent:kDollars];
+    NSInteger centValue = [pickerView selectedRowInComponent:KCents];
+    self.priceLabel.text = [NSString stringWithFormat:@"$%d.%02d", dollarValue, centValue];
+}
+
+@end
