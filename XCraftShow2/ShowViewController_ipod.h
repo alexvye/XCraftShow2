0442531b787cb8d94164c14fa35ad0a8adcb4666
Peter Chase <cchasep@hotmail.com>
2012-09-18 18:15:09 -0300

    Updated UI

--- XCraftShow2/ShowViewController_ipod.h
+++ XCraftShow2/ShowViewController_ipod.h
@@ -12,17 +12,18 @@
 @interface ShowViewController_ipod : UIViewController <UIPickerViewDataSource, UIPickerViewDelegate>{
     IBOutlet UIDatePicker* datePicker;
     IBOutlet UIPickerView* feePicker;
-    IBOutlet UITextField* feeTextField;
+    IBOutlet UIButton* feeButton;
     IBOutlet UITextField* nameTextField;
 }
 
 @property(nonatomic,retain) IBOutlet UIDatePicker* datePicker;
 @property(nonatomic,retain) IBOutlet UIPickerView* feePicker;
-@property(nonatomic,retain) IBOutlet UITextField* feeTextField;
+@property(nonatomic,retain) IBOutlet UIButton* feeButton;
 @property(nonatomic,retain) IBOutlet UITextField* nameTextField;
 @property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
 @property (strong, nonatomic) Show* passedShow;
 
+- (IBAction)changeFee:(id)sender;
 - (IBAction)cancelShow:(id)sender;
 - (IBAction)saveShow:(id)sender;
 
